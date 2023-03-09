b268ee591bac1fb22adb1ef8bb810f7747cd7188
Victor78970513 <yer29.chok@gmail.com>
2023-03-08 21:51:56 -0400

    order changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:local_auth/local_auth.dart';
-
-import 'package:local_auth_android/local_auth_android.dart';
-import 'package:local_auth_ios/local_auth_ios.dart';
-import 'package:securepassword/screens/home_screen.dart';
 
 void main() => runApp(const MyApp());
 
