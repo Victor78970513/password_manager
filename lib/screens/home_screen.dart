e9e9681f2d7cd2ca20f89ca8d96de1eda1804249
Victor <Yer59.chok@gmail.com>
2023-03-12 11:33:52 -0400

    New Password Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:securepassword/model/password.dart';
+import 'package:securepassword/screens/new_password.dart';
 import 'package:securepassword/widgets/card_password.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -31,14 +32,24 @@ class HomeScreen extends StatelessWidget {
                     final password = Password.fromDocumentSnapshot(document);
                     passwords.add(password);
                   }
-                  return Center(
-                    child: GridView.count(
-                        physics: const BouncingScrollPhysics(),
-                        crossAxisCount: 2,
-                        children: List.generate(passwords.length, (index) {
-                          final password = passwords[index];
-                          return CardPassword(password: password);
-                        })),
+                  return SingleChildScrollView(
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                          maxHeight: MediaQuery.of(context).size.height),
+                      child: GridView.count(
+                          physics: const BouncingScrollPhysics(),
+                          crossAxisCount: 2,
+                          childAspectRatio: 1.2,
+                          children:
+                              List.generate(passwords.length + 1, (index) {
+                            if (index < passwords.length) {
+                              final password = passwords[index];
+                              return CardPassword(password: password);
+                            } else {
+                              return AddPassword();
+                            }
+                          })),
+                    ),
                   );
                 } else {
                   return const CircularProgressIndicator();
@@ -47,5 +58,50 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
         ));
+  }
+}
+
+class AddPassword extends StatelessWidget {
+  const AddPassword({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+          context, MaterialPageRoute(builder: (_) => NewPassword())),
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: const Color(0xFFD9D9E3),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Color(0xff007AFF)),
+              child: const Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+            const Spacer(),
+            const Text(
+              "create",
+              style: TextStyle(
+                color: Color(0xff007AFF),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/screens/new_password.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewPassword extends StatelessWidget {
+  const NewPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("hola bb"),
+        ],
+      ),
+    );
+  }
+}
